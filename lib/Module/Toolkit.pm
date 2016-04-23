66324da78e09857021113676e4dab7df96a7f468
Tadeusz “tadzik” Sośnierz <tadeusz.sosnierz@onet.pl>
2016-04-23 12:59:03 +0200

    Move dist-from-location to M::T itself. Add --nodeps (-d) to redpanda. Bump version

--- lib/Module/Toolkit.pm
+++ lib/Module/Toolkit.pm
@@ -4,6 +4,7 @@ use Module::Toolkit::Fetcher;
 use Module::Toolkit::Installer;
 use TAP;
 use File::Find;
+use JSON::Fast;
 
 has $.ecosystem
     handles <project-list get-project get-dependencies>
@@ -21,6 +22,8 @@ has CompUnit::Repository @repos
         CompUnit::RepositoryRegistry.repository-for-name($_)
     });
 
+#= Check if a C<$dist>ribution is currently installed. If this returns
+#= True, you should be able to C<install()> it without C<:force>
 method is-installed(Distribution $dist) {
     so any(@repos).resolve(
         CompUnit::DependencySpecification.new(
@@ -31,11 +34,13 @@ method is-installed(Distribution $dist) {
     ) or so any(@repos).prefix.child('dist').child($dist.id).IO.e;
 }
 
+#= Fetch a given C<$dist>ribution, store in C<$to>
 multi method fetch(Distribution $dist, IO::Path() $to) {
     my $url = $dist.source-url // $dist.support<source>;
     $.fetcher.fetch($url, $to);
 }
 
+#= Fetch a distribution from a given C<$url>, store in C<$to>
 multi method fetch(Str $url, IO::Path() $to) {
     $.fetcher.fetch($url, $to);
 }
@@ -45,6 +50,9 @@ class Sink is IO::Handle {
     method flush    { }
 }
 
+#= Run tests for a distribution in a given directory
+#= TAP output will be written to C<$output> if supplied,
+#= ignored otherwise
 method test(IO::Path() $where, :$output = Sink.new) {
     temp $*CWD = chdir($where);
     return True unless $*CWD.child('t').IO.d;
@@ -59,4 +67,47 @@ method test(IO::Path() $where, :$output = Sink.new) {
     ).run(@tests);
 
     $run.result.get-status eq 'PASS'
+}
+
+#= Find the path to any valid metadata file in a given directory
+method find-meta-file(IO::Path() $dir) {
+    if $dir.child('META6.json').f {
+        return $dir.child('META6.json')
+    }
+    if $dir.child('META.info').f {
+        return $dir.child('META.info')
+    }
+}
+
+#= Obtain a Distribution object for a given C<$location>, which can
+#= be whatever Module::Toolkit::Fetcher may be able to handle.
+#=
+#= You have to supply a $tmpdir since it may be necessary to download
+#= the distribution into a temporary location in order to read its
+#= metadata. It may or may not be used, and you should rely on the
+#= source-url attribute rather than the C<$tmpdir> you passed in.
+method dist-from-location(Str $location, IO::Path() $tmpdir) {
+    try {
+        self.fetch($location, $tmpdir.IO);
+        CATCH { default {
+            return False;
+        }}
+    }
+
+    my $meta = self.find-meta-file($tmpdir.IO);
+    if $meta {
+        my $dist = from-json slurp $meta;
+        my @dep  = |($dist<depends>:delete);
+        my @bdep = |($dist<build-depends>:delete);
+        my @tdep = |($dist<test-depends>:delete);
+
+        return Module::Toolkit::Distribution.new(
+            |$dist,
+            depends       => @dep,
+            build-depends => @bdep,
+            test-depends  => @tdep,
+            source-url    => $tmpdir,
+        );
+    }
+    return False;
 }
